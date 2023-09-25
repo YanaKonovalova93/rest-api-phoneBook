bd7b6c67dac3ab22b4f82e82b75e5774ccd69631
YanaKonovalova93 <118968627+YanaKonovalova93@users.noreply.github.com>
2023-09-25 12:11:49 +0300

    +

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -5,24 +5,26 @@ import User from "../models/User.js";
 
 const { JWT_SECRET } = process.env;
 
-const authenticate = async (req, res, next) => {
-  const { authorization } = req.headers;
-  const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
-    throw HttpError(401, "Not authorized");
-  }
 
-  try {
-    const { id } = jwt.verify(token, JWT_SECRET);
-    const user = await User.findById(id);
-    if (!user || !user.token) {
-      throw HttpError(401, "Not authorized");
-    }
-    req.user = user;
-    next();
-  } catch {
+const authenticate = async(req, res, next) =>{
+const {authorization} = req.headers;
+const [bearer, token] = authorization.split(" ");
+if(bearer !== "Bearer") {
+throw HttpError(401, "Not authorized");
+}
+
+try{
+  const {id} = jwt.verify(token, JWT_SECRET);
+  const user = await User.findById(id);
+  if(!user || !user.token){
     throw HttpError(401, "Not authorized");
   }
-};
+  req.user = user;
+  next();
+}
+catch{
+    throw HttpError(401, "Not authorized");
+}
+}
 
 export default ctrlWrapper(authenticate);
